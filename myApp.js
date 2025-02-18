88df660fcd36a620aa5d7deca9cec3020abbf5d1
Lucas Godoy <lucasgodoyjor@gmail.com>
2025-02-18 06:17:35 -0300

    added simple search

--- myApp.js
+++ myApp.js
@@ -1,10 +1,15 @@
+import { pesquisaSimples } from './pesquisaSimples.js';
 import { pesquisaBinaria } from './pesquisaBinaria.js';
 import readline from 'readline';
+const myList = [1,3,5,7,9];
 console.log(" ");
 console.log("----------- Bem vindo ao MyApp! -----------");
 console.log(" ");
+console.log(`Array de exemplo: ${myList}`);
+console.log(" ");
 console.log("Buscas disponíveis:");
-console.log("1 - Busca Binária");
+console.log("1 - Busca Simples");
+console.log("2 - Busca Binária");
 console.log(" ");
 
 const rl = readline.createInterface({
@@ -12,17 +17,27 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-let minhaLista = [1,3,5,7,9];
+
 
 rl.question('Qual o tipo de busca deseja realizar? ', (numero1) => {
     const numeroDaBusca = parseInt(numero1);
     if(numeroDaBusca === 1){
         console.log(" ");
+        console.log("Busca Simples");
+        console.log(" ");
+        rl.question('Qual o item deseja buscar? ', (numero2) => {
+            const numero = parseInt(numero2);
+            console.log(`Index do item buscado no array: ${pesquisaSimples(myList, numero)}`);
+            rl.close();
+        });
+    }
+    else if(numeroDaBusca === 2){
+        console.log(" ");
         console.log("Busca Binária");
         console.log(" ");
         rl.question('Qual o item deseja buscar? ', (numero2) => {
             const numero = parseInt(numero2);
-            console.log(pesquisaBinaria(minhaLista, numero));
+            console.log(`Index do item buscado no array: ${pesquisaBinaria(myList, numero)}`);
             rl.close();
         });
     }
