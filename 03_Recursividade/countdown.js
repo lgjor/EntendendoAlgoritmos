e4c7357de887a17eae3976d5ba4b9856e29e589d
Lucas Godoy <lucasgodoyjor@gmail.com>
2025-03-04 07:37:08 -0300

    valid argument verification and round if is float

--- 03_Recursividade/countdown.js
+++ 03_Recursividade/countdown.js
@@ -1,3 +1,35 @@
+/**
+ * Contagem regressiva de forma iterativa e recursiva
+ * @param {*} n Número inteiro a ser contado de forma regressiva
+ * @returns Imprime a contagem regressiva 2x (iterativa e recursiva)
+ */ 
+const argv = process.argv;
+let n = argv[2];
+
+
+// Verifica se o argumento é um inteiro válido maior que zero
+if (isNaN(n)) {
+    console.error('Erro: O argumento não é um número.');
+  } else if (n <= 0) {
+    console.error('Erro: O argumento deve ser maior que zero.');
+  } else {
+    // Caso o argumento seja válido, converte para número
+    n = Number(argv[2]);
+    // Verifica se o número é inteiro e arredonda caso não seja
+    if (!Number.isInteger(n)) {
+        n = Math.round(n);
+        console.log(`Argumento válido arredondado: ${n}.`);
+    }
+    else {
+        console.log(`Argumento válido: ${n}.`);
+    }
+    console.log(`Inciando contagem regressiva...`);
+    console.log("Regressiva");
+    countDown(n);
+    console.log("Recursiva");
+    recursiveCountdown(n);
+  }
+
 function countDown(n) {
     for (let i = n; i >= 0; i--) {
         console.log(i);
@@ -12,10 +44,6 @@ function recursiveCountdown(n) {
     }
     else {
          // caso recursivo
-        contagemRegressivaRecursiva(n - 1);
+         recursiveCountdown(n - 1);
     }
 }
-console.log("Regressiva");
-countDown(10);
-console.log("Recursiva");
-recursiveCountdown(10);
