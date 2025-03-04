d4ad4ff2b85264b6be14f418dd9648388d9ae961
Lucas Godoy <lucasgodoyjor@gmail.com>
2025-03-04 09:56:47 -0300

    added maximum square divider

---
+++ 04-Quick_sort/MaximumSquareDivider.js
@@ -0,0 +1,20 @@
+const altura = 1680;
+const largura = 640;
+
+function MaximumSquareDivider(altura, largura) {
+    let menor = 0;
+    let maior = 0;
+    if ( largura > altura ) {
+        largura = largura -  altura;
+        return MaximumSquareDivider (altura, largura);
+    }
+    else if ( altura > largura ) {
+        altura = altura - largura;
+        return MaximumSquareDivider (altura, largura);
+    }
+    else {
+        return `Larger square to evenly divide the area: ${altura} x ${largura}`;
+    }
+}
+
+console.log(MaximumSquareDivider(altura, largura));
